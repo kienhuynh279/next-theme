7460fa88568092836e4d486e042cf8451811a1d6
kienhuynh279 <kienhuynh279@gmail.com>
2023-04-21 10:30:45 +0700

    source base

---
+++ src/components/chart.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import dynamic from "next/dynamic";
+import { Props } from "react-apexcharts";
+
+export const Chart = dynamic<Props>(() => import("react-apexcharts"), {
+  ssr: false,
+  loading: () => null,
+});
